bea13bbb20a703dd830da5cf65122f36d5809505
99x-ismailn <ismailN@99x.io>
2023-12-17 13:28:24 +0530

    commited with build

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import  './assets/main.css'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+:root {
+  --clr-primary-5: pink;
+  --clr-primary-10: blue;
+  --radius: 5px;
+  --spacing: 2px;
+  --transition: all 0.3s linear;
+
+}
+
+
+</style>
+
+
---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/todo',
+      name: 'todo',
+      component: () => import('../views/TodoView.vue')
+    },
+    {
+      path: '/socials',
+      name: 'socials',
+      component: () => import('../views/SocialsView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('../views/NotFoundView.vue')
+    }
+  ]
+})
+
+export default router
